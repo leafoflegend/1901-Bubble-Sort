18d2aa7c2aa8ae34a275ca06fd20074a435dbbf4
Eliot Szwajkowski <eliotpszw@gmail.com>
2019-01-15 15:21:31 -0500

    BubbleSort Repo Init.

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+type BubbleSort<T> = (toSort: T[]) => T[];
+const bubbleSort: BubbleSort<number> = (numbers: number[]) => {
+  let unsorted = false;
+  const copiedNumbers = numbers.slice();
+
+  for (let i = 0; i < copiedNumbers.length - 1; ++i) {
+    const currentNum = copiedNumbers[i];
+    const nextNum = copiedNumbers[i + 1];
+    if (currentNum > nextNum) {
+
+      unsorted = true;
+      copiedNumbers[i] = nextNum;
+      copiedNumbers[i + 1] = currentNum;
+    }
+  }
+
+  if (unsorted) return bubbleSort(copiedNumbers);
+
+  return copiedNumbers;
+};
+
+module.exports = bubbleSort;
---
+++ tests/index.spec.ts
@@ -0,0 +1,7 @@
+const bubbleSort = require('../src/index');
+
+describe('BubbleSort', () => {
+  test('Properly sorts simple cases', () => {
+     expect(bubbleSort([1, 4, 2, 3, 5])).toEqual([1, 2, 3, 4, 5]);
+  })
+});
